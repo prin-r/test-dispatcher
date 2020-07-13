54e78639b6230a43753a04d4ef7d33a670862d50
prin-r <q@bandprotocol.com>
2020-07-13 21:12:32 +0700

    add cache

---
+++ packages/acala-indexer/index.ts
@@ -0,0 +1,16 @@
+import Indexer from '@open-web3/indexer';
+import dotenv from 'dotenv';
+import { types as acalaTypes } from '@acala-network/types';
+
+dotenv.config();
+
+const run = async (): Promise<void> => {
+  const dbUrl = process.env.DB_URI as string;
+  const wsUrl = process.env.WS_URL || 'wss://node-6640517791634960384.jm.onfinality.io/ws';
+  const indexer = await Indexer.create({ dbUrl, wsUrl, types: acalaTypes, sync: true });
+  await indexer.start();
+};
+
+run().catch((err) => {
+  console.error(err);
+});
